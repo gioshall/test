307c9ff39a73c480aef25b1a06d09685b001d688
Gio Liao <gio.liao@nogle.com>
2016-08-05 17:40:44 +0800

    fix

---
+++ binary-gio/js/script.js
@@ -0,0 +1,21 @@
+function timePickerOnClick() {
+  document.getElementById('time-picker').style.display = 'block';
+}
+function hideTimePicker() {
+  document.getElementById('time-picker').style.display = 'none';
+}
+function gotoDetail() {
+  //console.log('go to detail');
+  window.location.href = 'detail.html';
+}
+
+// side menu
+
+$('#nav-icon').click(function(){
+	$(this).toggleClass('open');
+	$('body, .wrapper, .side-menu').toggleClass('active');
+});
+
+$('input[type="checkbox"]').on('change', function() {
+	$('input[type="checkbox"]').not(this).prop('checked', false);
+});
