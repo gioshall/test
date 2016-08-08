6b8677763d69b63cd8dbd02496b6fb50fedf5f2d
Gio Liao <gio.liao@nogle.com>
2016-08-08 12:27:17 +0800

    ＯＫＯＫ

--- binary-mobile/js/script.js
+++ binary-mobile/js/script.js
@@ -28,3 +28,45 @@ $('#nav-icon').click(function(){
 $('input[type="checkbox"]').on('change', function() {
 	$('input[type="checkbox"]').not(this).prop('checked', false);
 });
+
+//transaction
+
+  function bidOverlay() {
+      $('#bid-overlay').attr('class','');
+  }
+
+  $('.bid-btn .lbtn, .bid-btn .rbtn').click(function(){
+    var ovlStyle = $(this).attr('comfirmOvl');
+    $('#bid-overlay').addClass(ovlStyle)
+  });
+
+
+  $('#tab02').click(function(){
+    $('#odd-even').show();
+    $('#big-small').hide();
+    $('.bid-tab li').removeClass('active');
+    $(this).addClass('active');
+  });
+  $('#tab03').click(function(){
+    $('#big-small').show();
+    $('#odd-even').hide();
+    $('.bid-tab li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  $('.bottom-nav li').click(function(){
+    $('.bottom-nav li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  function navSwitch(e) {
+    $('.bottom-nav li.' + e ).click(function(){
+    $('.main-wrap').hide();
+    $('.main-wrap.' + e ).show();
+  });
+  };
+
+  
+  var chartHeight = $(window).height() - 332;
+  //console.log(chartHeight);
+  $('.main-wrap').css('height',chartHeight)
