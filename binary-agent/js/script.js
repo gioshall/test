dbb5f7d3a3daa9a483005ed1015e78f07a4e74cb
Gio Liao <gio.liao@nogle.com>
2016-08-12 17:34:27 +0800

    CMS

--- binary-agent/js/script.js
+++ binary-agent/js/script.js
@@ -1,3 +1,3 @@
-$('input[type="checkbox"]').on('change', function() {
-	$('input[type="checkbox"]').not(this).prop('checked', false);
+$('.menu-list input[type="checkbox"]').on('change', function() {
+  $('.menu-list input[type="checkbox"]').not(this).prop('checked', false);
 });
